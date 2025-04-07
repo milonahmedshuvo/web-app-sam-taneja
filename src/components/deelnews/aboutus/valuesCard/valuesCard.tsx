a456e16ea498aa9ad15e70ef0e73f08ac2dd530f
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2025-04-07 17:58:33 +0600

    mission vission section code modify

--- src/components/deelnews/aboutus/valuesCard/valuesCard.tsx
+++ src/components/deelnews/aboutus/valuesCard/valuesCard.tsx
@@ -8,12 +8,16 @@ interface ValuesCardProps {
 
 export function ValuesCard({ title, description, icon: Icon }: ValuesCardProps) {
   return (
-    <div className="bg-white p-8 rounded-lg shadow-sm flex flex-col items-center text-center pb-24">
+    <div  className="bg-white p-8 rounded-lg shadow-sm flex flex-col pb-24">
       <div className="w-16 h-16 rounded-full bg-white flex items-center justify-center mb-6">
-        <Icon className="w-10 h-10 text-[#4863AC]" />
+        <Icon className="w-20 h-20 text-[#4863AC]" />
       </div>
-      <h2 className="text-2xl font-bold text-gray-800 mb-4">{title}</h2>
-      <p className="text-gray-600">{description}</p>
+
+
+      <div>
+          <h2 className="text-2xl !font-bold text-gray-800 !mb-6">{title}</h2>
+           <p className="text-gray-600">{description}</p>
+      </div>
     </div>
   )
 }
